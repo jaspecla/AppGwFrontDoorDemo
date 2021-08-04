7f03433fbecacb8aa23b5d7dbd1b2e46e7b80e8c
Jason Specland <jaspecla@microsoft.com>
2021-08-04 13:24:01 -0400

    Add project files.

---
+++ AppGwFrontDoorDemo.API/Controllers/SearchController.cs
@@ -0,0 +1,31 @@
+﻿using AppGwFrontDoorDemo.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppGwFrontDoorDemo.API.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class SearchController : Controller
+  {
+
+    private ColorDataAccess ColorData { get; set; }
+
+    public SearchController(ColorDataAccess colorData)
+    {
+      ColorData = colorData;
+    }
+
+    [HttpGet("{searchString}", Name = "Search")]
+    public async Task<IActionResult> Index(string searchString)
+    {
+      var colors = await ColorData.SearchColorsAsync(searchString);
+
+      return Ok(colors);
+
+    }
+  }
+}
---
+++ AppGwFrontDoorDemo.API/Data/ColorDataAccess.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppGwFrontDoorDemo.API.Data
+{
+  public class ColorDataAccess
+  {
+    private IDataAccess DataAccess { get; set; }
+
+    public ColorDataAccess(IDataAccess dataAccess)
+    {
+      DataAccess = dataAccess;
+
+    }
+
+    public async Task<IEnumerable<string>> SearchColorsAsync(string searchString)
+    {
+      // PURPOSELY SETTING UP SQL INJECTION ATTACK
+      var query = $"SELECT Color FROM Colors WHERE Color LIKE '%{searchString}%'";
+
+      var reader = await DataAccess.GetDataReaderAsync(query, null);
+
+      var colors = new List<string>();
+
+      while (await reader.ReadAsync())
+      {
+        var color = reader["Color"].ToString();
+        colors.Add(color);
+      }
+
+      return colors;
+
+    }
+
+  }
+}
